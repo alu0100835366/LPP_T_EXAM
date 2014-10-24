ab33386456c43d386d6688ffeeb773e8943e4fcd
SUAREZ SPINOSI <alu4745@cc119.(none)>
2014-10-24 11:07:17 +0100

    Creada la expectativa que almacena una pregunta

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,4 +2,9 @@ require "lib/test.rb"
 
 describe Test do
   
+  describe "#Almacenamiento de Pregunta" do
+    it "Se guarda correctamente una pregunta" do
+      @p1.q.should eq("hola?")
+    end
+  end
 end
