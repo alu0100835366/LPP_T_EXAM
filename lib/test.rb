4d6a0c80702e06481979254192ec72191425f6de
SUAREZ SPINOSI <alu4745@cc119.(none)>
2014-10-24 12:56:22 +0100

    Solucionada la expectativa que comprueba la respuesta correcta

--- lib/test.rb
+++ lib/test.rb
@@ -1,12 +1,24 @@
 class Test
-  attr_reader :q, :a1, :a2, :a3, :a4, :t
-  def initialize(q,a1,a2,a3,a4,t)
-    @q, @a1, @a2, @a3, @a4, @t = q, a1, a2, a3, a4, t
+  attr_reader :q, :a1, :a2, :a3, :a4, :t, :a
+  def initialize(q,a1,a2,a3,a4,t,a)
+    @q, @a1, @a2, @a3, @a4, @t, @a= q, a1, a2, a3, a4, t, a
   end
   def showq()
     print "Pregunta: ", q, "\n"
   end
   def showa()
     print "\nA) ", a1, "\nB) ", a2, "\nC) ", a3, "\nD) ", a4
+  end
+  def ask()
+    puts "\n\nElige una de las 4 opciones."
+    if @a == 1
+      @a = a1
+    elsif @a == 2
+      @a = a2
+    elsif @a == 3
+     @a = a3
+    elsif @a == 4
+     @a = a4
+    end
   end
 end
