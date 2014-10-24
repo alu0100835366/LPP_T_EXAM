d41bd3a61353f52214a95b69b0706e1fbe74884f
SUAREZ SPINOSI <alu4745@cc198.(none)>
2014-10-24 15:26:42 +0100

    Ultimas modificaciones en clase

--- lib/test.rb
+++ lib/test.rb
@@ -1,24 +1,27 @@
 class Test
-  attr_reader :q, :a1, :a2, :a3, :a4, :t, :a
-  def initialize(q,a1,a2,a3,a4,t,a)
-    @q, @a1, @a2, @a3, @a4, @t, @a= q, a1, a2, a3, a4, t, a
+  attr_reader :q, :a1, :t, :a
+  def initialize(q,a1,t,a)
+
+    @q, @a1, @t, @a= q, a1, t, a
   end
   def showq()
     print "Pregunta: ", q, "\n"
   end
   def showa()
-    print "\nA) ", a1, "\nB) ", a2, "\nC) ", a3, "\nD) ", a4
+    for i in(0...4)
+      print "\nA) ",  a1[i]
+    end
   end
   def ask()
     puts "\n\nElige una de las 4 opciones."
     if @a == 1
-      @a = a1
+      @a = a1[0]
     elsif @a == 2
-      @a = a2
+      @a = a1[1]
     elsif @a == 3
-     @a = a3
+     @a = a2[2]
     elsif @a == 4
-     @a = a4
+     @a = a1[3]
     end
     
     print "\nLa respuesta correcta es: ", a
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,7 +2,7 @@ require "lib/test.rb"
 
 describe Test do
   before :each do
-    @q1 = Test.new("Hola?", "Si", "No", "Quizas", "Puede", 3)
+    @q1 = Test.new("Hola?", ["Si", "No", "Quizas", "Puede"], 3, 1)
   end
   
   describe "#Almacenamiento de Pregunta" do
@@ -12,22 +12,22 @@ describe Test do
   end
   describe "#Almanecamiento de Respuesta 1" do
     it "Se guarda correctamente la primera respuesta" do
-      @q1.a1.should eq("Si")
+      @q1.a1[0].should eq("Si")
     end
   end
   describe "#Almanecamiento de Respuesta 2" do
     it "Se guarda correctamente la segunda respuesta" do
-      @q1.a2.should eq("No")
+      @q1.a1[1].should eq("No")
     end
   end
   describe "#Almanecamiento de Respuesta 3" do
     it "Se guarda correctamente la tercero respuesta" do
-      @q1.a3.should eq("Quizas")
+      @q1.a1[2].should eq("Quizas")
     end
   end
   describe "#Almanecamiento de Respuesta 4" do
     it "Se guarda correctamente la cuarta respuesta" do
-      @q1.a4.should eq("Puede")
+      @q1.a1[3].should eq("Puede")
     end
   end
   describe "#Almanecamiento de Respuesta Correcta" do
@@ -38,6 +38,17 @@ describe Test do
   describe "#Mostrar Pregunta" do
     it "Se muestra la pregunta correctamente" do
       @q1.showq()
+    end
+  end
+  describe "#Mostrar Respuestas" do
+    it "Se muestran las respuestas correctamente" do
+      @q1.showa()
+    end
+  end
+  describe "#Respuesta Correcta" do
+    it "Se comprueba que la respuesta es la correcta" do
+      @q1.ask()
+      @q1.a.should eq @q1.a1[0]
     end
   end
 end
