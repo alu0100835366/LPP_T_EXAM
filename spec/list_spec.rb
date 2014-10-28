86b7c04d13ae13cb4626ce9f066292e3908f6d11
Marcos Luis <alu0100777086@ull.edu.es>
2014-10-28 21:35:44 +0000

    Trying specs

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,1 +1,9 @@
 require "lib/list.rb"
+describe "#Almacenamiento de nodo" do
+	it "Se guarda correctamente un nodo" do
+		@test = List.new(3)
+		@test.push(4)
+		@test.push(5)
+		@test.pop
+	end
+end
