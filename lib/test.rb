c4bcb54367ddc5b48f684064b2e8167ef0df08d1
SUAREZ SPINOSI <alu4745@cc160.(none)>
2014-11-07 14:05:51 +0000

    another minox fix

--- lib/test.rb
+++ lib/test.rb
@@ -4,11 +4,10 @@ class Test
 
     @q, @a1, @t, @a= q, a1, t, a
   end
-  
   def to_s()
     print "Pregunta: ", q, "\n"
     for i in(0...a1.size)
-      print "\nA) ",  a1[i]
+      print "\n ", i+1, ")",  a1[i]
     end
     puts "\n\nElige una de las opciones."
      for i in(0...a1.size)
@@ -18,4 +17,22 @@ class Test
      end
     print "\nLa respuesta correcta es: ", a, "\n\n\n\n"
   end
+end
+
+class BTest < Test
+  def initialize(q,a1,t,a)
+    super(q,a1,t,a)
+  end
+  def to_s()
+  print "Pregunta: ", q, "\n"
+      print "\nA) ",  a1[0]
+      print "\nB) ",  a1[1]
+    puts "\n\nElige una de las opciones."
+       if @a == 1
+      @a = a1[0]
+       else
+      @a = a1[1]
+       end
+    print "\nLa respuesta correcta es: ", a, "\n\n\n\n"
+  end 
 end
