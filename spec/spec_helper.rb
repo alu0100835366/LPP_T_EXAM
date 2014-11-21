fa86431ce7d92a511c3eb8aa6d5d61b666b4035d
Marcos Luis <alu0100777086@ull.edu.es>
2014-11-21 10:37:54 +0000

    Coveralls, added files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
   end
+  require ’coveralls’
+  Coveralls.wear!
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
